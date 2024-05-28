38f7fd86e257a7d882a4e62aa2a17e10509aa49c
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2024-05-28 17:53:13 +0000

    added admin panel config

--- config/server.js
+++ config/server.js
@@ -1,5 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
+  admin: {
+    url: '/admin',
+    serveAdminPanel: true,
+  },
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
