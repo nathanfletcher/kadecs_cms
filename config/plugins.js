a838fd7c91ba1da7b6171ac48899672f2040c79e
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2024-05-28 17:25:59 +0000

    created vercel config

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,12 @@
 module.exports = ({ env }) => ({
-    "vercel-deploy": {
-      enabled: true,
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        env('DEPLOY_HOOK_VERCEL','https://my-deployhook.vercel.com'),
+      apiToken: env('API_TOKEN_VERCEL','my-vercel-api-token'),
+      appFilter: env('APP_FILTER_VERCEL','my-vercel-app-filter'),
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
-  });
+  },
+});
