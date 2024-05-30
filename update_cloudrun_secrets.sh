07f58dab8eeebb83765fbc436fb74f4bfaea2990
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2024-05-30 23:44:03 +0000

    fixed gh cli parameter error

--- update_cloudrun_secrets.sh
+++ update_cloudrun_secrets.sh
@@ -4,13 +4,13 @@
 service_name=${CLOUD_RUN_SERVICE_NAME}
 
 # Get all repository secrets
-secrets=$(gh secret list --json | jq -r '.[].name')
+ghsecrets=$(gh secret list --json name | jq -r '.[].name')
 
 # Loop through each secret
-for secret in $secrets; do
+for ghsecret in $ghsecrets; do
   # Decode the secret value
-  decoded_value=$(echo -n "${!secret}" | base64 -d)
-  
+  decoded_value=$(echo -n "${!ghsecret}" | base64 -d)
+
   # Update environment variable for Cloud Run service (skip non-base64 secrets)
   if [[ ! -z "$decoded_value" ]]; then
     gcloud run services update "$service_name" \
